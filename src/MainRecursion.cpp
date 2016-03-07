8f5b1e8d802cba64892b0a6ab0abc20cdcdfe581
Abhijith Ravuri <KH9126@kitspl.com>
2016-03-07 11:00:03 +0530

    Initialcommit

--- src/MainRecursion.cpp
+++ src/MainRecursion.cpp
@@ -5,7 +5,16 @@
 
 int main(){
 	
+	//Check NSnipers
+
+	//Check CountPairs Sample Code 
+
+	//Check Steps Sample Code
+
+	//Check Spiral Sample Code
+
 	//Check Maze Sample Code
+
 	/*
 	int maze[4][3] = {
 		{ 0, 0, 1 },
@@ -21,62 +30,6 @@ int main(){
 	printf("%d\n", check);
 	*/
 
-	//Check NSnipers
-
-	/*
-	int n = 4;
-	int board[4][4];
-	int i = 0, j = 0;
-	for (i = 0; i < n; i++){
-		for (j = 0; j < n; j++){
-			board[i][j] = 0;
-		}
-	}
-
-	if (solve_nsnipers(&board[0][0], n) == 0)
-	{
-		printf("Solution does not exist");
-		return false;
-	}
-
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-			printf(" %d ", board[i][j]);
-		printf("\n");
-	}
-	*/
-
-	//Check CountPairs Sample Code 
-	/*
-	printf("%d\n", count_pairs_wrapper("zzzaz", 5));
-	*/
-
-	//Check Steps Sample Code
-	/*
-	printf("%d\n", get_steps(7));
-	*/
-
-	//Check Spiral Sample Code
 	
-	/*
-	int **input_array = (int **)malloc(2 * sizeof(int *));
-	for (int i = 0; i < 2; i++)
-		input_array[i] = (int *)malloc(2 * sizeof(int));
-	
-	input_array[0][0] = 3;
-	input_array[0][1] = 4;
-	input_array[1][0] = 5;
-	input_array[1][1] = 6;
-
-	
-	int *arr = spiral(2, 2, (int **)input_array);
-
-	for (int i = 0; i < 4; i++){
-		printf("%d ", arr[i]);
-	}
-	printf("\n");
-
-	*/
 	return 0;
 }
